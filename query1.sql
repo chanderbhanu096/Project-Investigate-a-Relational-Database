99bc800cc270f817718028c790b89e99489d6284
Chander Bhanu <chanderbhanu096@gmail.com>
2023-04-12 13:39:57 +0200

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -3,14 +3,14 @@ select t1.name as category, count(*) AS movie_count, sum(count(*)) over() as tot
   (count(*))*100 / sum(count(*)) over() as percentage    /* here count(*) will calcualate the total movies in one category and sum is 
                                                             allowing us to perform sum operation across the whole data set filtered from the subquery */
   
-from (select fi.title, c.name
-  from film fi
-  join film_category f 
-  on f.film_id=fi.film_id
+from (select f.title, c.name
+  from film f
+  join film_category fc 
+  on fc.film_id=f.film_id
   join inventory i 
-  on i.film_id=f.film_id
+  on i.film_id=fc.film_id
   join category c 
-  on f.category_id=c.category_id
+  on fc.category_id=c.category_id
   join rental r 
   on r.inventory_id=i.inventory_id
   where c.name in('Animation','Children','Classics','Comedy','Family','Music')) AS t1
