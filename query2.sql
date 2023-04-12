79ad75a2f276ad2ef52a437b8134367f5a9eda85
Chander Bhanu <chanderbhanu096@gmail.com>
2023-04-12 13:52:55 +0200

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,5 +1,4 @@
-/* a table with the movie titles and divide them into 4 levels (first_quarter, second_quarter, third_quarter, and final_quarter)
-based on the quartiles (25%, 50%, 75%) of the average rental duration(in the number of days) for movies across all categories?*/
+/* To find a every movie rental duration percentile in their relevant category*/
 SELECT f.title, c.name, f.rental_duration,
 NTILE(4) OVER (ORDER BY f.rental_duration) AS standard_quartile
 FROM film f
