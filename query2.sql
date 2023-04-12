dbc989d75cfb3af63805e10e0bfb08b350177f41
Chander Bhanu <chanderbhanu096@gmail.com>
2023-04-12 21:02:58 +0200

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -7,3 +7,14 @@ ON f.film_id=fc.film_id
 JOIN category c
 ON c.category_id=fc.category_id
 WHERE c.name IN ('Animation','Children','Classics','Comedy','Family','Music')
+
+/* based on the category which movie  was rented most and for how much duration*/
+SELECT c.name as category, f.rental_duration,count(*) as total_movies
+FROM film f
+JOIN film_category fc
+ON f.film_id=fc.film_id
+JOIN category c
+ON c.category_id=fc.category_id
+WHERE c.name IN ('Animation','Children','Classics','Comedy','Family','Music')
+group by 1,2
+order by 3 desc,2 desc 
